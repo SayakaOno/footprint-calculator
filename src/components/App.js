c4a78a1c3ad317860f69a8a03a53253a68f35c15
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-04 16:34:45 -0700

    [Sidebar] add  methods to manage state

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,26 @@ import Sidebar from './Sidebar';
 import './reset.css';
 
 class App extends React.Component {
-  state = { currentPosition: null, destination: null };
+  state = { currentLocation: null, destination: null, transportation: '' };
 
   componentDidMount = () => {};
 
   render() {
     return (
       <div id='outer-container' className='app'>
-        <Sidebar onChange={destination => this.setState({ destination })} />
+        <Sidebar
+          onChange={(fieldName, destination) =>
+            this.setState({ [fieldName]: destination })
+          }
+          onClick={transportation => this.setState({ transportation })}
+          currentLocation={this.state.currentLocation}
+          destination={this.state.destination}
+          transportation={this.state.transportation}
+        />
         <h1>Carbon Footprint Calculator</h1>
+        <p>
+          {this.state.currentLocation}, {this.state.destination}
+        </p>
       </div>
     );
   }
