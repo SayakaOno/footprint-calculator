2b8253f1fa977aa4ab9503a743f0972a4e11fa9c
Kelly <xiaohandang@gmail.com>
2019-05-05 11:34:01 -0700

    added footprint calculation

--- src/components/Maps/map.jsx
+++ src/components/Maps/map.jsx
@@ -17,22 +17,42 @@ export class MapContainer extends React.Component {
         DirectionsDisplay.setMap(map);
         DirectionsDisplay.setPanel(document.getElementById('DirectionsPanel'));
 
-        var transitPromise = getFootprint('TRANSIT');
-        var drivingPromise = getFootprint('DRIVING');
+        var footprintPromises = [
+            getFootprint('TRANSIT'),
+            getFootprint('DRIVING'),
+            getFootprint('WALKING'),
+            getFootprint('BICYCLING')
+        ];
 
-        Promise.all([transitPromise, drivingPromise])
-        .then((arr) => {
-            console.log(arr);
-            var bestRoute = arr.reduce((acc, curr, idx, self) => {
-                if ((typeof acc.footprint === 'undefined') || (curr.footprint < acc.footprint)) {
-                    return {footprint: curr.footprint, response: curr.response};
-                } else {
+        Promise.all(footprintPromises)
+            .then((arr) => {
+                console.log(arr);
+
+                var footprints = arr.reduce((acc, curr) => {
+                    acc[curr.travelMode] = curr;
                     return acc;
+                }, {});
+
+                var bestRoute = footprints['DRIVING'];
+
+                // if walking to feasible
+                if (footprints['WALKING'].duration < 30) {
+                    bestRoute = footprints['WALKING'];
+                } else {
+                    if (footprints['BICYCLING'].duration < 45) {
+                        bestRoute = footprints['BICYCLING'];
+                    } else {
+                        if ((footprints['TRANSIT'].footprint <= footprints['DRIVING'].footprint)
+                        && ((footprints['TRANSIT'].duration - footprints['DRIVING'].duration) < 30)) {
+                            bestRoute = footprints['TRANSIT'];
+                        }
+                    }
                 }
-            }, {footprint: undefined, response: {}});
-            console.log(bestRoute);
-            DirectionsDisplay.setDirections(bestRoute.response);
-        });
+
+                console.log(bestRoute);
+                DirectionsDisplay.setDirections(bestRoute.response);
+                window.alert(`${bestRoute.travelMode}: footprint: ${bestRoute.footprint}kg`);
+            });
 
         function getFootprint(travelMode) {
             return new Promise((resolve, reject) => {
@@ -51,22 +71,28 @@ export class MapContainer extends React.Component {
                             return parseFloat(route.legs[0].distance.text.split(' ')[0], 10);
                         });
                         const durations = response.routes.map(route => {
-                            var hours = parseFloat((route.legs[0].duration.text.match(/[\d]+ hours/) || [0])[0], 10);
-                            
-                            var minutes = parseFloat((route.legs[0].duration.text.match(/[\d]+ mins/) || [0])[0], 10);
-                            
-                            var total = hours*60 + minutes;
+                            var hours = parseFloat((route.legs[0].duration.text.match(/[\d]+ hour/) || [0])[0], 10);
+
+                            var minutes = parseFloat((route.legs[0].duration.text.match(/[\d]+ min/) || [0])[0], 10);
+
+                            var total = hours * 60 + minutes;
                             return total;
                         });
 
                         const minDistance = Math.min(...distances);
                         const minDuration = Math.min(...durations);
                         var carbonEmissionInKg;
-                        if (travelMode === 'DRIVING') {
-                            carbonEmissionInKg = 0.251 * minDistance;
-                        } else {
-                            // TRANSIT
-                            carbonEmissionInKg = 0.200 * minDistance;
+
+                        switch (travelMode) {
+                            case 'DRIVING':
+                                carbonEmissionInKg = 0.251 * minDistance;
+                                break;
+                            case 'TRANSIT':
+                                carbonEmissionInKg = 0.129 * minDistance;
+                                break;
+                            default:
+                                carbonEmissionInKg = 0;
+                                break;
                         }
 
                         var footprintInfo = {
