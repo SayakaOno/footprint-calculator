74a76b567cc48269ae1bb569448c3c7972c9be93
Kelly <xiaohandang@gmail.com>
2019-05-05 12:20:32 -0700

    moved call to api in onClick

--- src/components/Maps/map.jsx
+++ src/components/Maps/map.jsx
@@ -8,15 +8,13 @@ export class MapContainer extends React.Component {
     //  - transit
     //  - walking
 
-    onReady(mapProps, map) {
+    onMapClicked(mapProps) {
+        const { google } = mapProps;
         const origin = this.props.origin;
         const destination = this.props.destination;
-        const { google } = mapProps;
         const DirectionsService = new google.maps.DirectionsService();
-        const DirectionsDisplay = new google.maps.DirectionsRenderer();
-        DirectionsDisplay.setMap(map);
-        DirectionsDisplay.setPanel(document.getElementById('DirectionsPanel'));
-
+        const DirectionsDisplay = this.directionsDisplay;
+    
         var footprintPromises = [
             getFootprint('TRANSIT'),
             getFootprint('DRIVING'),
@@ -112,6 +110,112 @@ export class MapContainer extends React.Component {
         }
     }
 
+    onReady(mapProps, map) {
+        // const origin = this.props.origin;
+        // const destination = this.props.destination;
+        const { google } = mapProps;
+        // const DirectionsService = new google.maps.DirectionsService();
+        const DirectionsDisplay = new google.maps.DirectionsRenderer();
+        DirectionsDisplay.setMap(map);
+        DirectionsDisplay.setPanel(document.getElementById('DirectionsPanel'));
+
+        this['directionsDisplay'] = DirectionsDisplay;
+
+        // var footprintPromises = [
+        //     getFootprint('TRANSIT'),
+        //     getFootprint('DRIVING'),
+        //     getFootprint('WALKING'),
+        //     getFootprint('BICYCLING')
+        // ];
+
+        // Promise.all(footprintPromises)
+        //     .then((arr) => {
+        //         console.log(arr);
+
+        //         var footprints = arr.reduce((acc, curr) => {
+        //             acc[curr.travelMode] = curr;
+        //             return acc;
+        //         }, {});
+
+        //         var bestRoute = footprints['DRIVING'];
+
+        //         // if walking to feasible
+        //         if (footprints['WALKING'].duration < 30) {
+        //             bestRoute = footprints['WALKING'];
+        //         } else {
+        //             if (footprints['BICYCLING'].duration < 45) {
+        //                 bestRoute = footprints['BICYCLING'];
+        //             } else {
+        //                 if ((footprints['TRANSIT'].footprint <= footprints['DRIVING'].footprint)
+        //                 && ((footprints['TRANSIT'].duration - footprints['DRIVING'].duration) < 30)) {
+        //                     bestRoute = footprints['TRANSIT'];
+        //                 }
+        //             }
+        //         }
+
+        //         console.log(bestRoute);
+        //         DirectionsDisplay.setDirections(bestRoute.response);
+        //         // window.alert(`${bestRoute.travelMode}: footprint: ${bestRoute.footprint}kg`);
+        //     });
+
+        // function getFootprint(travelMode) {
+        //     return new Promise((resolve, reject) => {
+
+        //         DirectionsService.route({
+        //             origin: origin,
+        //             destination: destination,
+        //             travelMode: travelMode,
+        //             provideRouteAlternatives: true,
+        //             transitOptions: {
+        //                 departureTime: new Date()
+        //             }
+        //         }, (response, status) => {
+        //             if (status === 'OK') {
+        //                 const distances = response.routes.map(route => {
+        //                     return parseFloat(route.legs[0].distance.text.split(' ')[0], 10);
+        //                 });
+        //                 const durations = response.routes.map(route => {
+        //                     var hours = parseFloat((route.legs[0].duration.text.match(/[\d]+ hour/) || [0])[0], 10);
+
+        //                     var minutes = parseFloat((route.legs[0].duration.text.match(/[\d]+ min/) || [0])[0], 10);
+
+        //                     var total = hours * 60 + minutes;
+        //                     return total;
+        //                 });
+
+        //                 const minDistance = Math.min(...distances);
+        //                 const minDuration = Math.min(...durations);
+        //                 var carbonEmissionInKg;
+
+        //                 switch (travelMode) {
+        //                     case 'DRIVING':
+        //                         carbonEmissionInKg = 0.251 * minDistance;
+        //                         break;
+        //                     case 'TRANSIT':
+        //                         carbonEmissionInKg = 0.129 * minDistance;
+        //                         break;
+        //                     default:
+        //                         carbonEmissionInKg = 0;
+        //                         break;
+        //                 }
+
+        //                 var footprintInfo = {
+        //                     travelMode: travelMode,
+        //                     duration: minDuration,
+        //                     distance: minDistance,
+        //                     footprint: carbonEmissionInKg,
+        //                     response: response
+        //                 };
+        //                 resolve(footprintInfo);
+        //             } else {
+        //                 window.alert('Directions request failed due to ' + status);
+        //                 reject('Directions request failed due to ' + status);
+        //             }
+        //         });
+        //     })
+        // }
+    }
+
     render() {
         return (
             <Map
@@ -121,7 +225,7 @@ export class MapContainer extends React.Component {
                     lat: 49.2606,
                     lng: -123.2460
                 }}
-                onClick={this.onMapClicked}
+                onClick={this.onMapClicked.bind(this)}
                 onReady={this.onReady.bind(this)}
             >
 
