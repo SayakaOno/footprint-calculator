f275b38415fd217d5a082c0d0fd0efcb4a68b802
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-04 17:17:59 -0700

    add Trivia component

---
+++ src/components/Trivia.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './trivia.css';
+
+class Trivia extends React.Component {
+  render() {
+    return (
+      <div class='trivia'>
+        <h2>Did you know?</h2>
+        <p>Trivia</p>
+      </div>
+    );
+  }
+}
+
+export default Trivia;
