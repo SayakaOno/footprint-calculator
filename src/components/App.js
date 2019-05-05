458444f56dd0ad4d3f5f9fc82de32c1567e62fa4
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-04 20:06:31 -0700

    * get current location * add styles

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,43 @@ import Sidebar from './Sidebar';
 import './reset.css';
 import Maps from './Maps/map';
 import Trivia from './Trivia';
+import './app.css';
 
 class App extends React.Component {
   state = { currentLocation: '', destination: '', transportation: '' };
 
-  componentDidMount = () => { };
+  componentDidMount = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.showPosition);
+    }
+  };
+
+  showPosition = position => {
+    this.setState({
+      currentLocation: `${position.coords.latitude},${
+        position.coords.longitude
+      }`
+    });
+  };
 
   render() {
     return (
       <div id='outer-container' className='app'>
-        <Sidebar
-          onChange={(fieldName, destination) =>
-            this.setState({ [fieldName]: destination })
-          }
-          onClick={transportation => this.setState({ transportation })}
-          currentLocation={this.state.currentLocation}
-          destination={this.state.destination}
-          transportation={this.state.transportation}
-        />
         <h1>Carbon Footprint Calculator</h1>
-        <Maps origin='UBC' destination='University of Victoria'>Maps</Maps>
+        <div className='container'>
+          <Sidebar
+            onChange={(fieldName, destination) =>
+              this.setState({ [fieldName]: destination })
+            }
+            onClick={transportation => this.setState({ transportation })}
+            currentLocation={this.state.currentLocation}
+            destination={this.state.destination}
+            transportation={this.state.transportation}
+          />
+          <div className='map'>
+            <Maps origin='UBC' destination='University of Victoria'>Maps</Maps>
+          </div>
+        </div>
         <Trivia />
       </div>
     );
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -4,7 +4,7 @@ import './trivia.css';
 class Trivia extends React.Component {
   render() {
     return (
-      <div class='trivia'>
+      <div className='trivia'>
         <h2>Did you know?</h2>
         <p>Trivia</p>
       </div>
