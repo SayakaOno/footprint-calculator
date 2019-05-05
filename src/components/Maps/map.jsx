7367c7c0aeae5150b05bb217aa990fdf6bf1d985
Kelly <xiaohandang@gmail.com>
2019-05-05 02:24:10 -0700

    refactored gotFootprint and selectively renders direction

--- src/components/Maps/map.jsx
+++ src/components/Maps/map.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {Map, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
- 
+import { Map, InfoWindow, GoogleApiWrapper } from 'google-maps-react';
+
 export class MapContainer extends React.Component {
     // travel modes:
     //  - driving
@@ -11,84 +11,105 @@ export class MapContainer extends React.Component {
     onReady(mapProps, map) {
         const origin = this.props.origin;
         const destination = this.props.destination;
-        const {google} = mapProps;
+        const { google } = mapProps;
         const DirectionsService = new google.maps.DirectionsService();
         const DirectionsDisplay = new google.maps.DirectionsRenderer();
         DirectionsDisplay.setMap(map);
         DirectionsDisplay.setPanel(document.getElementById('DirectionsPanel'));
-        DirectionsService.route({
-            origin: origin,
-            destination: destination,
-            travelMode: 'TRANSIT',
-            provideRouteAlternatives: true,
-            transitOptions: {
-                departureTime: new Date()
-            }
-        }, (response, status) => {
-            if (status === 'OK') {
-                DirectionsDisplay.setDirections(response);
-                console.log(response.routes.length);
-                console.log(response.routes[0].legs[0].distance.text);
-                console.log(response.routes[0].legs[0].duration.text);
 
-                const distances = response.routes.map(route => {
-                    return parseFloat(route.legs[0].distance.text.split(' ')[0], 10);
-                });
-                const durations = response.routes.map(route => {
-                    return route.legs[0].duration.text;    
-                });
+        var transitPromise = getFootprint('TRANSIT');
+        var drivingPromise = getFootprint('DRIVING');
 
-                const minDistance = Math.min(...distances);
-                const carbonEmissionInKg = 0.251 * minDistance;
-
-                console.log(distances);
-                console.log(durations);
-                var footprintInfo = {
-                    travelMode: "",
-                    duration: "",
-                    distance: "",
-                    footprint: carbonEmissionInKg
-                };
-                console.log(`${footprintInfo.footprint}kg`);
-                // debugger;
-            } else {
-                window.alert('Directions request failed due to ' + status);
-            }
+        Promise.all([transitPromise, drivingPromise])
+        .then((arr) => {
+            console.log(arr);
+            var bestRoute = arr.reduce((acc, curr, idx, self) => {
+                if ((typeof acc.footprint === 'undefined') || (curr.footprint < acc.footprint)) {
+                    return {footprint: curr.footprint, response: curr.response};
+                } else {
+                    return acc;
+                }
+            }, {footprint: undefined, response: {}});
+            console.log(bestRoute);
+            DirectionsDisplay.setDirections(bestRoute.response);
         });
+
+        function getFootprint(travelMode) {
+            return new Promise((resolve, reject) => {
+
+                DirectionsService.route({
+                    origin: origin,
+                    destination: destination,
+                    travelMode: travelMode,
+                    provideRouteAlternatives: true,
+                    transitOptions: {
+                        departureTime: new Date()
+                    }
+                }, (response, status) => {
+                    if (status === 'OK') {
+                        const distances = response.routes.map(route => {
+                            return parseFloat(route.legs[0].distance.text.split(' ')[0], 10);
+                        });
+                        const durations = response.routes.map(route => {
+                            var hours = parseFloat((route.legs[0].duration.text.match(/[\d]+ hours/) || [0])[0], 10);
+                            
+                            var minutes = parseFloat((route.legs[0].duration.text.match(/[\d]+ mins/) || [0])[0], 10);
+                            
+                            var total = hours*60 + minutes;
+                            return total;
+                        });
+
+                        const minDistance = Math.min(...distances);
+                        const minDuration = Math.min(...durations);
+                        var carbonEmissionInKg;
+                        if (travelMode === 'DRIVING') {
+                            carbonEmissionInKg = 0.251 * minDistance;
+                        } else {
+                            // TRANSIT
+                            carbonEmissionInKg = 0.200 * minDistance;
+                        }
+
+                        var footprintInfo = {
+                            travelMode: travelMode,
+                            duration: minDuration,
+                            distance: minDistance,
+                            footprint: carbonEmissionInKg,
+                            response: response
+                        };
+                        resolve(footprintInfo);
+                    } else {
+                        window.alert('Directions request failed due to ' + status);
+                        reject('Directions request failed due to ' + status);
+                    }
+                });
+            })
+        }
     }
 
-    // onMapClicked(mapProps, map, clickEvent){
-    //     debugger;
-    // }
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={14}
+                initialCenter={{
+                    lat: 49.2606,
+                    lng: -123.2460
+                }}
+                onClick={this.onMapClicked}
+                onReady={this.onReady.bind(this)}
+            >
 
-    // centerMoved(mapProps, map) {
-    //     debugger;
-    // }
-
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        initialCenter={{
-            lat: 49.2606,
-            lng: -123.2460
-        }}
-        onClick={this.onMapClicked}
-        onReady={this.onReady.bind(this)}
-      >
- 
-        {/* <Marker onClick={this.onMarkerClick}
+                {/* <Marker onClick={this.onMarkerClick}
                 name={'Current location'} /> */}
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-        <div></div>
-        </InfoWindow>
-      </Map>
-    );
-  }
+
+                <InfoWindow onClose={this.onInfoWindowClose}>
+                    <div></div>
+                </InfoWindow>
+            </Map>
+        );
+    }
 }
- 
+
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDaV21H2rXlro4Dpob6crEO9YbqQxmvV-I'
+    apiKey: 'AIzaSyDaV21H2rXlro4Dpob6crEO9YbqQxmvV-I'
 })(MapContainer)
