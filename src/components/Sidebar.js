3ee4056fae64f88baac025005566658f677a0d21
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-05 10:08:42 -0700

    add images to code

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import './sidebar.css';
+import biking from '../images/biking.png';
+import driving from '../images/driving.png';
+import transit from '../images/transit.png';
+import walking from '../images/walking.png';
+import biking_icon from '../images/biking_icon.png';
+import driving_icon from '../images/driving_icon.png';
+import transit_icon from '../images/transit_icon.png';
+import walking_icon from '../images/walking_icon.png';
+import title from '../images/title.png';
+import trivia from '../images/trivia.png';
 
 class Sidebar extends React.Component {
   state = {
@@ -17,7 +27,9 @@ class Sidebar extends React.Component {
   render() {
     return (
       <div className='sidebar bm-menu'>
-        <h1>Carbon Footprint Calculator</h1>
+        <h1>
+          <img alt='carbon emissions calculator' src={title} />
+        </h1>
         <label htmlFor='currentLocation'>A</label>
         <input
           id='currentLocation'
@@ -38,17 +50,29 @@ class Sidebar extends React.Component {
           placeholder='ending point'
         />
         <ul className='buttons'>
-          <li onClick={() => this.props.onClick('car')}>
-            <img alt='car' src='' />
+          <li onClick={() => this.props.onClick('driving')}>
+            <span>
+              <img alt='' src={driving_icon} />
+            </span>
+            <img alt='driving' src={driving} />
           </li>
-          <li onClick={() => this.props.onClick('bus')}>
-            <img alt='bus' src='' />
+          <li onClick={() => this.props.onClick('transit')}>
+            <span>
+              <img alt='' src={transit_icon} />
+            </span>
+            <img alt='transit' src={transit} />
           </li>
-          <li onClick={() => this.props.onClick('bicycle')}>
-            <img alt='bicycle' src='' />
+          <li onClick={() => this.props.onClick('biking')}>
+            <span>
+              <img alt='' src={biking_icon} />
+            </span>
+            <img alt='biking' src={biking} />
           </li>
-          <li onClick={() => this.props.onClick('walk')}>
-            <img alt='walk' src='' />
+          <li onClick={() => this.props.onClick('walking')}>
+            <span>
+              <img alt='' src={walking_icon} />
+            </span>
+            <img alt='walking' src={walking} />
           </li>
         </ul>
         <div id='DirectionsPanel' />
